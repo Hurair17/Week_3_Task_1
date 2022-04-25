741df833ed1a3bb0d9fa7b602c5cd9df87651f56
“Hurair17” <[hurairmohammad17@gmail.com]>
2022-04-25 17:16:11 +0500

    Stop at Cenetering Register Icon

--- lib/ui/screens/registration_auth/signup/signup.dart
+++ lib/ui/screens/registration_auth/signup/signup.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
+import 'package:week_3_task/core/constants/color.dart';
 
 class SignUp extends StatelessWidget {
   const SignUp({Key? key}) : super(key: key);
@@ -7,7 +9,49 @@ class SignUp extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       body: SingleChildScrollView(
-        child: Text('hewl'),
+        child: SafeArea(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.start,
+            children: [
+              Padding(
+                padding: EdgeInsets.only(top: 20.h, left: 10.h),
+                child: Container(
+                  decoration: BoxDecoration(
+                      borderRadius: BorderRadius.circular(20),
+                      color: lightgreen),
+                  child: IconButton(
+                    onPressed: () {},
+                    icon: Icon(
+                      Icons.arrow_back_ios,
+                      color: darkgren,
+                    ),
+                    splashRadius: 20.r,
+                  ),
+                ),
+              ),
+              SizedBox(
+                height: 40.h,
+              ),
+              Stack(
+                children: [
+                  Column(
+                    children: [
+                      Center(
+                        child: Text(
+                          'Register',
+                          style: TextStyle(
+                              color: darkgren,
+                              fontSize: 40.sp,
+                              fontWeight: FontWeight.bold),
+                        ),
+                      )
+                    ],
+                  )
+                ],
+              )
+            ],
+          ),
+        ),
       ),
     );
   }
